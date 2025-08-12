bff3f8b061b82b43a1433903ce6f6cba0bdd58cb
Polat Kargılı <54178145+aliora@users.noreply.github.com>
2025-08-12 14:26:04 +0300

    clean

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const RelayControl = require('./RelayControl');
-
-const relayControl = new RelayControl('MSR-CH340');
-//relayControl.triggerRelays('192.168.1.111', 9747,'1');
-relayControl.triggerRelays('192.168.99.94', 9747,'1');
-//relayControl.triggerRelays('100.8100.76.66.361.65.92', 9747,'1',100);sudo usermod -aG gpio $USER
